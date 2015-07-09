562df12a2454df09f34c1ad33ecdef1a6818c9cf
zx <mac1@zxlmac1deMini.lan>
2015-07-09 10:40:47 +0800

    Initial Commit

---
+++ XingHuiWeiGang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XingHuiWeiGang
+//
+//  Created by zx on 15/7/9.
+//  Copyright (c) 2015年 zxl－mac1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
